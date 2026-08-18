8a0175b6c15af09531ac32caf6a3436fb7951cec
f4kbm2hgpz-cmd <f4kbm2hgpz@privaterelay.appleid.com>
2026-08-19 01:44:49 +0530

    Remove play button functionality from script.js
    Removed DOMContentLoaded event listener and play button functionality.

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const playBtn = document.getElementById("playBtn");
-
-  if (playBtn) {
-    playBtn.addEventListener("click", function () {
-      const iframe = document.querySelector(".youtube-wrap iframe");
-
-      if (iframe) {
-        iframe.scrollIntoView({
-          behavior: "smooth",
-          block: "center"
-        });
-      }
-    });
-  }
-});
+console.log("Script loaded");
