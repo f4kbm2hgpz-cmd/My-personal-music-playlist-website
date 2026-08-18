4649a62b866aae418fe8425357854aefbbbe6681
f4kbm2hgpz-cmd <f4kbm2hgpz@privaterelay.appleid.com>
2026-08-19 01:54:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,13 @@
-let player;
-let playerReady = false;
+document.addEventListener("DOMContentLoaded", function () {
+  const playBtn = document.getElementById("playBtn");
+  const player = document.getElementById("youtubePlayer");
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player("youtubePlayer", {
-    events: {
-      onReady: function () {
-        playerReady = true;
-
-        const playBtn = document.getElementById("playBtn");
-        const prevBtn = document.getElementById("prevBtn");
-        const nextBtn = document.getElementById("nextBtn");
-
-        if (playBtn) {
-          playBtn.onclick = function () {
-            if (playerReady) {
-              player.playVideo();
-            }
-          };
-        }
-
-        if (prevBtn) {
-          prevBtn.onclick = function () {
-            if (playerReady) {
-              player.previousVideo();
-            }
-          };
-        }
-
-        if (nextBtn) {
-          nextBtn.onclick = function () {
-            if (playerReady) {
-              player.nextVideo();
-            }
-          };
-        }
-      }
-    }
-  });
-}
-
-const youtubeScript = document.createElement("script");
-youtubeScript.src = "https://www.youtube.com/iframe_api";
-document.head.appendChild(youtubeScript);
+  if (playBtn && player) {
+    playBtn.addEventListener("click", function () {
+      player.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+      });
+    });
+  }
+});
