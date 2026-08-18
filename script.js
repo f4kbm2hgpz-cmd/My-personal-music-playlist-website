d683de31bc494dba92152828118db2db5c683360
f4kbm2hgpz-cmd <f4kbm2hgpz@privaterelay.appleid.com>
2026-08-19 01:46:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Script loaded");
+const playlistId = "PLlBTRvaC0XtKnzRLR5m7dc1S58gk_nZJ_";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const playBtn = document.getElementById("playBtn");
+  const iframe = document.querySelector(".youtube-wrap iframe");
+
+  if (playBtn && iframe) {
+    playBtn.addEventListener("click", function () {
+      iframe.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+      });
+    });
+  }
+});
